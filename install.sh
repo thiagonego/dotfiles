d73f4cf8d3f915c18c7401b922d0b8425162dd8f
Thiago Sampaio <thiagonego@gmail.com>
2011-10-26 22:27:04 -0200

    Pequenas mudanças para minhas necessidades

--- install.sh
+++ install.sh
@@ -1,26 +1,27 @@
+echo "Criando os arquivos Dotfiles em /User/thiagosampaio"
 cp files/bash_profile ~/.bash_profile
-cp files/caprc ~/.caprc
 cp files/gitconfig ~/.gitconfig
 cp files/irbrc ~/.irbrc
 cp files/inputrc ~/.inputrc
-cp files/gemrc ~/.gemrc
 cp files/npmrc ~/.npmrc
-cp files/rvmrc ~/.pryrc
+cp files/pryrc ~/.pryrc
 cp files/gitignore ~/.gitignore
 
-mkdir ~/bin
-
-curl http://github.com/paulhammond/webkit2png/raw/master/webkit2png > ~/.bash/bin/webkit2png
-chmod +x ~/.bash/bin/webkit2png
-
+mkdir -p ~/bin
 mkdir -p ~/.ssh
 cp files/sshconfig ~/.ssh/config
 
 sudo mkdir -p /etc/bash_completion
 
+echo "Lendo o .bash_profile"
 source ~/.bash_profile
 
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
+echo "Fim"
 
-open files/IR_Black.terminal
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+# defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
+
+# open files/IR_Black.terminal
+
+#Quando eu colocar meu repositório de Gems volto essa configurações
+# cp files/gemrc ~/.gemrc
